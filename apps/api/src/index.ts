7e72bd1aa30d785966e97b96d27fb563273fbed7
VASAPOL RITTIDEAH <70092356+vasapol123@users.noreply.github.com>
2023-02-03 02:36:11 +0700

    initial commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.APP_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
+});
---
+++ apps/api/webpack.config.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+import WebpackShellPluginNext from 'webpack-shell-plugin-next';
+import nodeExternals from 'webpack-node-externals';
+import { Configuration as WebpackConfiguration } from 'webpack';
+
+const config: WebpackConfiguration = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new NodePolyfillPlugin(),
+    new WebpackShellPluginNext({
+      onBuildEnd: {
+        scripts: ['npx nx run:dev api'],
+      }
+    }),
+  ],
+  externals: [nodeExternals()],
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['.ts', '.js', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  stats: {
+    colors: true,
+  },
+};
+export default config;
